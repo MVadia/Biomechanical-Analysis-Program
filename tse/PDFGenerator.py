615d7605d40919691ef7636f565843cbae3393e5
mvadia <mvadia02@gmail.com>
2023-05-10 19:01:25 +0100

    generate final report

--- tse/PDFGenerator.py
+++ tse/PDFGenerator.py
@@ -10,15 +10,19 @@ from docx2pdf import convert
 from docx.shared import RGBColor
 
 
-document = Document('existing_template.docx')
+
 
 
 
 
 ##Code here to handle patient data and selected graphs, add to functions
-def get_graph_data(document, name, address, age, contact, graph_selected):
-    print (name, address, age, contact, graph_selected)
-    
+def get_graph_data(name, address, age, contact, graph_selected):
+    document = Document('existing_template.docx')
+    name = str(name)
+    address = str(address)
+    age = str(age)
+    contact = str(contact)
+
     p = document.add_paragraph('Name: ' + name + ' \n Age: '+ age +
                                '\n Address: ' + address + '\n Contact Num: '+ contact)
     first_run = p.runs[0]
@@ -30,68 +34,21 @@ def get_graph_data(document, name, address, age, contact, graph_selected):
     p.space_before = Inches(3)
     p.right_indent = Inches(7) - (p.paragraph_format.left_indent or Inches(0)) - (p.paragraph_format.first_line_indent or Inches(0)) - (p.paragraph_format.space_before or Pt(0))
 
+
+    for graph_name, selected in graph_selected.items():
+        if selected:
+            image_path = graph_name
+            width = Inches(6)
+            height = Inches(5)
+            image = document.add_picture(image_path)
+            left = Inches(2)
+            top = Inches(4)
+            image.left = left
+            image.top = top
+            image.width = width
+            image.height = height
     
 
-    # add the image to the document
-    image_path = 'Knee_Flexion.png'
-
-    # set the position and size of the image
-    width = Inches(4)
-    height = Inches(3)
-    image = document.add_picture(image_path)
-    left = Inches(2)
-    top = Inches(4)
-    image.left = left
-    image.top = top
-    image.width = width
-    image.height = height
-
-
-    # add the image to the document
-    image_path = 'Elbow_Flexion.png'
-    image = document.add_picture(image_path)
-
-    # get the image dimensions
-    image_width = image.width
-    image_height = image.height
-
-    # set the position of the image in the page (in EMU)
-    left = Inches(2)
-    top = Inches(4)
-
-    # set the image size
-    width = Inches(4)
-    height = Inches(3)
-
-    # set the position and size of the image
-    image.left = left
-    image.top = top
-    image.width = width
-    image.height = height
-
-    p = document.add_paragraph("wdwdqdwdqw")
-
-    # add the image to the document
-    image_path = 'Pelvis_Flexion.png'
-    image = document.add_picture(image_path)
-
-    # get the image dimensions
-    image_width = image.width
-    image_height = image.height
-
-    # set the position of the image in the page (in EMU)
-    left = Inches(2)
-    top = Inches(4)
-
-    # set the image size
-    width = Inches(4)
-    height = Inches(3)
-
-    # set the position and size of the image
-    image.left = left
-    image.top = top
-    image.width = width
-    image.height = height
 
     # save the document
     document.save('Modified.docx')
@@ -103,5 +60,13 @@ def get_graph_data(document, name, address, age, contact, graph_selected):
     convert(input_file, output)
 
 
+graph_selected = { "Pre_Knee_Flexion.png": True,     
+                            "Pre_Elbow_Flexion.png": True,
+                           "Pre_Pelvis_Flexion.png": True,
+                           "Post_Knee_Flexion.png": True,
+                           "Post_Elbow_Flexion.png": True,
+                           "Post_Pelvis_flexion.png": True}
 
-get_graph_data(document, name="name", address="fhk3qw", age="12", contact="293742", graph_selected="jda" )
+
+
+get_graph_data(name="name", address="address", age="age", contact="Contact", graph_selected= graph_selected)
